cea3cf6106a5bd55a57303123ee30eade4b161df
mhracki <mhracki@gmail.com>
2021-01-26 22:43:57 +0100

    prettier

--- components/home/category/categories-list.jsx
+++ components/home/category/categories-list.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Category from './category';
+import Category from "./category";
 
-
-const CategoriesList = ({menu}) => {
-
-  return <div className="flex categories-list" >
-      {menu.map(x=>{
-          return <Category key={x.name} img={x.img} name={x.name}/>})}
-  </div>;
+const CategoriesList = ({ menu }) => {
+  return (
+    <div className="flex categories-list">
+      {menu.map((x) => {
+        return <Category key={x.name} img={x.img} name={x.name} />;
+      })}
+    </div>
+  );
 };
 export default CategoriesList;
