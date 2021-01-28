b8546765aec875154fd960ffe6b6db7ca0f47796
mhracki <mhracki@gmail.com>
2021-01-29 00:34:41 +0100

    env + mini_img

--- components/home/category/categories-list.jsx
+++ components/home/category/categories-list.jsx
@@ -5,7 +5,7 @@ const CategoriesList = ({ menu }) => {
   return (
     <div className="flex categories-list">
       {menu.map((x) => {
-        return <Category key={x.name} img={x.img} name={x.name} />;
+        return <Category key={x.name} img={x.mini_img} name={x.name} />;
       })}
     </div>
   );
