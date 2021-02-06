9069032487c0cb504ef1a80db67512dfbe3a1f15
mhracki <mhracki@gmail.com>
2021-02-06 19:52:50 +0100

    footer icon refactor

--- components/home/footer.jsx
+++ components/home/footer.jsx
@@ -7,28 +7,47 @@ const Footer = ({}) => {
     <div className="footer-container">
       <div className="socials flex">
         <Link href="https://www.facebook.com/pages/category/Restaurant/Pycha-Micha-107978534326545/">
-          <a id="face" target="_blank">
-
+          <a target="_blank">
+            <div id="face"></div>
+            <Image
+              src="/images/social-svg/facebook.svg"
+              alt="facebook"
+              height="100px"
+              width="100px"
+            />
           </a>
         </Link>
         <Link href="tel:+48-33-852-12-12">
-          <a id="call-us">
-           
+          <a>
+            <div id="call-us"></div>
+            <Image
+              src="/images/social-svg/phonecall.svg"
+              alt="Call-us"
+              height="100px"
+              width="100px"
+            />
           </a>
         </Link>
         <Link href="https://www.instagram.com/restauracjapycha/">
-          <a id="insta" target="_blank">
-
+          <a target="_blank">
+            <div id="insta"></div>
+            <Image
+              src="/images/social-svg/instagram.svg"
+              alt="instagram"
+              height="100px"
+              width="100px"
+            />
           </a>
         </Link>
       </div>
       <div class="author flex">
-        
-        <p> © Designed & Developed by  
+        <p>
+          {" "}
+          © Designed & Developed by
           <Link href="https://hracki.pl">
-             <a>MH</a>
+            <a>MH</a>
           </Link>
-          </p> 
+        </p>
       </div>
     </div>
   );
